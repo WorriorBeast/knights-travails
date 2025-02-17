ac76a620be0345960824f0ad61aaab3e7797ce56
Emmanuel Sanchez <128549074+WorriorBeast@users.noreply.github.com>
2025-02-16 22:48:01 -0800

    Find the shortest path from a to b and return path

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@ import { Knight } from './knight-travails';
 
 const knight1 = new Knight();
 
-console.log(knight1.isWithinBoard(-7, -5)); // Expected output: false
-console.log(knight1.isWithinBoard(7, -5)); // Expected output: false
-console.log(knight1.isWithinBoard(7, 5)); // Expected output: true
-console.log(knight1.isWithinBoard(4, 1)); // Expected output: true
+console.log(knight1.shortestPath([-2, 0], [0, 0]));
+/*
+Expected output: -1;
+*/
+
+console.log(knight1.shortestPath([0, 0], [3, 3]));
+/* 
+Expected output: [[0, 0], [1, 2], [3, 3]]
+*/
+
+console.log(knight1.shortestPath([3, 3], [0, 0]));
+/*
+Expected output: [[3, 3], [2, 1], [0, 0]]
+*/
+
+console.log(knight1.shortestPath([0, 0], [7, 7]));
+
+/*
+Expected output: [[0, 0], [1, 2], [2, 4], [3, 6], [5, 7], [6, 5], [7, 7]]
+*/
